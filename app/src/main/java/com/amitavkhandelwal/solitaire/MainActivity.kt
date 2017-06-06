78ceba55e42d0676862a384e0811725889b8a388
Amitav Khandelwal <amitav.khandelwal@gmail.com>
2017-06-06 17:55:44 +0530

    Initial commit with empty project

---
+++ app/src/main/java/com/amitavkhandelwal/solitaire/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.amitavkhandelwal.solitaire
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        var counter = 0
+    }
+}
